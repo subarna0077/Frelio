fe003f742e2fc56982696076e263a384941fb16e
subarna0077 <sumansapkota0099@gmail.com>
2026-05-12 11:59:46 +0545

    Added dummy project pages

--- src/features/projects/pages/Projects.tsx
+++ src/features/projects/pages/Projects.tsx
@@ -1,10 +1,19 @@
 import {Button} from '@mui/material'
 import { useProjectStore } from '../stores/ProjectStore'
 import { AddProjectModal } from '../components/AddProjectModal'
+import { useGetProjects } from '../hooks/useGetProjects'
+import {List, ListItem, ListItemButton, ListItemText} from '@mui/material'
+import {useNavigate} from 'react-router-dom'
 export const Projects = () => {
 
     const setModal = useProjectStore(state=> state.setOpenModal) 
     const openModal = useProjectStore(state=> state.openModal)
+
+    const navigate = useNavigate()
+
+    const {data: projects} = useGetProjects()
+    console.log(projects)
+
     
 
   return (
@@ -13,6 +22,21 @@ export const Projects = () => {
 
         <Button onClick={()=> setModal(true)}>Add new project</Button>
         {openModal && <AddProjectModal/>}
+
+        <List>
+          {projects?.map(
+            project=> 
+            <ListItem key={project.id}>
+              <ListItemButton onClick={()=> navigate(`/projects/${project.id}`)} >
+                <ListItemText>{project.title}</ListItemText>
+              </ListItemButton>
+
+            </ListItem>
+
+          )}
+        </List>
+
+        
       
     </div>
   )
