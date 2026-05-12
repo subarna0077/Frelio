3aaec36cb88f85aa22a7e94dfb57a8257609ae8f
subarna0077 <sumansapkota0099@gmail.com>
2026-05-12 18:38:21 +0545

    Added Query devtools.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+       
         <App />
       </QueryClientProvider>
     </BrowserRouter>
