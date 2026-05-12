0144325aa97a500debb50abe0a67fcc0cdcdd0e3
subarna0077 <sumansapkota0099@gmail.com>
2026-05-12 11:56:38 +0545

    Added new hook to get all projects.

---
+++ src/features/projects/hooks/useGetProjects.ts
@@ -0,0 +1,22 @@
+import {useQuery} from '@tanstack/react-query'
+import { supabase } from '../../../lib/supabase'
+import { useAuthStore } from '../../auth/stores/authStore'
+import type { Project } from '../types/types'
+
+export const useGetProjects = ()=>{
+
+    const user = useAuthStore(state=> state.user)
+
+    const getProjects = async ()=>{
+        const {data: projects, error} = await supabase.from('projects').select('*').eq('user_id', user?.id);
+        if(error) throw error;
+        // why we throw here and no return
+        return projects ?? [];
+    }
+
+    return useQuery<Project []>({
+        queryKey: ['projects'],
+        queryFn: getProjects,
+        
+    })
+}
