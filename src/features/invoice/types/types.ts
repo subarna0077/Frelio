9f4831de83a292697371ad318fad549966210001
subarna0077 <sumansapkota0099@gmail.com>
2026-05-19 19:21:38 +0545

    Created invoice type

---
+++ src/features/invoice/types/types.ts
@@ -0,0 +1,22 @@
+export type InvoiceStatus = 'draft' | 'sent' | 'paid' | 'overdue'
+import type { Client } from "../../clients/types/types"
+
+export interface Invoice {
+    id: string,
+    user_id: string,
+    client_id: string,
+    project_id: string,
+    invoice_number: string,
+    status: InvoiceStatus,
+    due_date: string,
+    created_at: string,
+    clients: Pick<Client, 'name'| 'id' | 'address' > | null,
+    total: number
+}
+
+export interface InvoiceItems {
+    id: string,
+    invoice_id: string,
+    description: string,
+    amount: number,
+}
