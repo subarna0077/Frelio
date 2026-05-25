f7bafd6b7d755ca6d7f9a845e31142e51cf23dee
subarna0077 <sumansapkota0099@gmail.com>
2026-05-26 00:08:13 +0545

    Deno function created to initiate a khalti payment initiation.

---
+++ supabase/functions/khalti-initiate/index.ts
@@ -0,0 +1,67 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { Resend } from "npm:resend";
+import { corsHeaders } from '../_shared/cors.ts'
+
+
+
+Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  try {
+    const { amount, purchaseOrderId, purchaseOrderName } = await req.json();
+
+    const supabase = createClient(
+      Deno.env.get("SUPABASE_URL")!,
+      Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
+    );
+
+    const khaltiKey = Deno.env.get('KHALTI_SECRET_KEY')
+
+    if(!khaltiKey) {
+      return new Response(
+        JSON.stringify({
+          error: 'Khalti secret key is not configured'
+        }), {status: 500, headers: {...corsHeaders, 'Content-Type': 'application/json'}}
+      )
+    }
+
+
+    const response = await fetch('https://dev.khalti.com/api/v2/epayment/initiate/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Key ${khaltiKey}`
+      },
+      body: JSON.stringify({
+        return_url: `http://localhost:5173/payment-success?orderID=${purchaseOrderId}`,
+        amount: amount,
+        purchase_order_id: purchaseOrderId,
+        purchase_order_name: purchaseOrderName,
+        website_url: 'http://localhost:5173'
+
+      })
+    })
+
+    if(!response.ok) {
+      const error = await response.json()
+      return new Response(
+        JSON.stringify({detail: error.detail ?? 'Khalti error'}),
+        {status: response.status, headers: {...corsHeaders, 'Content-Type': 'application/json'}}
+      )
+    }
+
+    const data = await response.json()
+
+    return new Response(
+      JSON.stringify(data),
+      {headers: {...corsHeaders, 'Content-Type': 'application/json'}}
+    )
+
+  } catch (err) {
+    return new Response(JSON.stringify({
+      error: err instanceof Error ? err.message : String(err)
+    }), {status: 500, headers: corsHeaders})
+  }
+});
