665d8ea060859eb25efa634c4b7f3730a116b764
subarna0077 <sumansapkota0099@gmail.com>
2026-05-26 15:13:32 +0545

    Updated types for payment support of invoice.

--- src/features/invoice/types/types.ts
+++ src/features/invoice/types/types.ts
@@ -1,5 +1,6 @@
 export type InvoiceStatus = 'draft' | 'sent' | 'paid' | 'overdue'
 import type { Client } from "../../clients/types/types"
+import type { Project } from "../../projects/types/types";
 
 export interface InvoiceItems {
     id: string,
@@ -18,12 +19,27 @@ export interface Invoice {
     status: InvoiceStatus,
     due_date: string,
     created_at: string,
-    clients: Pick<Client, 'name'| 'id' | 'address' > | null,
-    total: number,
-    invoice_items: InvoiceItems[],
-    public_token: string,
     sent_at: string,
     viewed_at: string
+
+    //payment fields
+    paid_at: string| null;
+    payment_method: 'khalti' | 'esewa' | 'bank' | 'cash' | null;
+    payment_reference: string | null;  // khalti's pidx goes here
+
+
+    //invoice content
+    total: number;
+    notes: string | null;
+
+    //joined relations
+    clients: Pick<Client, 'name'| 'id' | 'address' > | null,
+    projects: Pick<Project, 'id' | 'title'> | null,
+    invoice_items: InvoiceItems[],
+
+    //portal
+    public_token: string,
+    
 }
 
 
