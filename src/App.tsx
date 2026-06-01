ea9b297e1040a364825631c3c716d763eb783093
subarna0077 <sumansapkota0099@gmail.com>
2026-06-01 11:59:42 +0545

    Handled the session when rendering the app. Removed local storage update from login hook to the authState change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
 import { AppRouter } from './router/AppRouter'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
+import { supabase } from './lib/supabase'
+import { useEffect } from 'react'
+import { useAuthStore } from './features/auth/stores/authStore'
 
 function App() {
+
+  const logout = useAuthStore(state => state.logout)
+  const setUser = useAuthStore(state => state.setUser)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (!session) {
+        logout()
+      }
+
+    })
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_OUT' || !session) logout();
+      if (event === 'SIGNED_IN') setUser({
+        id: session!.user.id,
+        name: session!.user.user_metadata.full_name,
+        email: session!.user.email!
+      });
+      if (event === 'TOKEN_REFRESHED') setUser({
+        id: session!.user.id,
+        name: session!.user.user_metadata.full_name,
+        email: session!.user.email!
+      })
+
+      return () => subscription.unsubscribe()
+
+    })
+  }, [])
+
+
   return (
     <>
       <ReactQueryDevtools />
