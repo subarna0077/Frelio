4f181855570d29f998e4906de39da33b5971733a
subarna0077 <sumansapkota0099@gmail.com>
2026-06-01 11:59:47 +0545

    Update useLogin.ts

--- src/features/auth/hooks/useLogin.ts
+++ src/features/auth/hooks/useLogin.ts
@@ -1,10 +1,7 @@
 import { useMutation } from '@tanstack/react-query'
 import type { LoginDataType } from '../types/auth'
-import { useAuthStore } from '../stores/authStore'
 import { supabase } from '../../../lib/supabase'
 export const useLogin = () => {
-
-    const setUser = useAuthStore(state => state.setUser)
 
     const loginFn = async (data: LoginDataType) => {
         const { data: authData, error: authError } = await supabase.auth.signInWithPassword({
@@ -19,15 +16,5 @@ export const useLogin = () => {
 
     return useMutation({
         mutationFn: loginFn,
-        onSuccess: (data) => {
-            const user = {
-                email: data.user.email ?? '',
-                id: data.user.id ?? '',
-                name: data.user.user_metadata.full_name ?? ''
-            }
-
-            const accessToken = data.session.access_token
-            setUser(user, accessToken)
-        },
     })
 }
