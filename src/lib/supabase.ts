f288133017c4f8f2ddac660837fa14b5d9028750
subarna0077 <sumansapkota0099@gmail.com>
2026-06-01 12:03:32 +0545

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-console.log(supabaseUrl, supabaseKey)
 
 export const supabase = createClient(
   supabaseUrl,
